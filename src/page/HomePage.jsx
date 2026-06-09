b70ade5bae7581be96f25678aabbfde317ff4f50
Juan Gonon <juanfegonon27@gmail.com>
2026-06-09 09:11:34 -0600

    homePage MovieGrid init

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -18,10 +18,17 @@ export const HomePage = () => {
     )
 
   return (
-    <div className='relative w-full min-h-[85vh] rounded-2xl md:min-h-[80vh] flex flex-col justify-end px-6 md:px-12 pb-16 overflow-hidden'>
-      {/* CONTENEDOR DEL TEXTO (ENCIMA) */}
-
-      <HeroCarousel key={moviesDay.id} movies={moviesDay} />
+    <div className='w-full min-h-screen   space-y-12 pb-12'>
+      <div className='relative w-full min-h-[85vh] rounded-2xl md:min-h-[80vh] flex flex-col justify-end px-6 md:px-12 pb-16 overflow-hidden'>
+        <HeroCarousel key={moviesDay.id} movies={moviesDay} />
+      </div>
+      <div className='px-6 md:px-12 space-y-12 relative z-20'>
+        <section>
+          <h2 className='text-2xl font-bold mb-6 font-headline uppercase tracking-tight'>
+            Popular Movies
+          </h2>
+        </section>
+      </div>
     </div>
   )
 }
