19364a93cacef0970afcaa66d01d8485be31a422
Juan Gonon <juanfegonon27@gmail.com>
2026-06-12 15:15:32 -0600

    custom hook useDebounce

---
+++ src/hooks/useDebounce.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+export const useDebounce = (value, delay = 500) => {
+  const [debounceValue, setDebounceValue] = useState(value)
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebounceValue(value)
+    }, delay)
+
+    return () => clearTimeout(handler)
+  }, [delay, value])
+  return {
+    debounceValue,
+  }
+}
