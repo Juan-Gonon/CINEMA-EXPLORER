3bdd7be432f05bcf3507884777bf04fea62acca9
Juan Gonon <juanfegonon27@gmail.com>
2026-06-12 11:22:55 -0600

    lazy loader swpper

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -11,6 +11,7 @@ export const MovieCard = ({ movie }) => {
           src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
           alt={movie?.title}
           className='w-full h-full object-cover opacity-85'
+          loading='lazy'
         />
       </div>
 
