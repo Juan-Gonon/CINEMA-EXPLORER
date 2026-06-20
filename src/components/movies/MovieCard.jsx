ebabc8be1416abaebb2e18c3322b69983e544805
Juan Gonon <juanfegonon27@gmail.com>
2026-06-20 10:16:14 -0600

    link movie card

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -1,19 +1,23 @@
+import { Link } from 'react-router-dom'
+
 export const MovieCard = ({ movie }) => {
   return (
     <div
       className='relative h-55 w-35  md:h-75 md:w-40 overflow-hidden hover:scale-110 transition
                 duration-300 ease-in-out'>
-      <div className='relative h-[80%] w-full overflow-hidden rounded-xs'>
-        <span className='absolute text-xs z-20 p-0.5 right-2.5 top-2 bg-[#2c2c2e] rounded-2xl'>
-          ⭐ {movie?.vote_average?.toFixed(1)}
-        </span>
-        <img
-          src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
-          alt={movie?.title}
-          className='w-full h-full object-cover opacity-85'
-          loading='lazy'
-        />
-      </div>
+      <Link to={`/movie/${movie?.id}`}>
+        <div className='relative h-[80%] w-full overflow-hidden rounded-xs'>
+          <span className='absolute text-xs z-20 p-0.5 right-2.5 top-2 bg-[#2c2c2e] rounded-2xl'>
+            ⭐ {movie?.vote_average?.toFixed(1)}
+          </span>
+          <img
+            src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
+            alt={movie?.title}
+            className='w-full h-full object-cover opacity-85'
+            loading='lazy'
+          />
+        </div>
+      </Link>
 
       <article className='flex flex-col'>
         <span className='text-center text-xs tracking-wider leading-tight'>
